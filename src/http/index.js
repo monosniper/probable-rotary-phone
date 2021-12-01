231ca6d932575e76d74e9ef512a7ac026075092f
Равиль <vanteateam@gmail.com>
2021-12-01 21:50:06 +0500

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:5000/api/';
+export const API_URL = 'http://vps-38679.vps-default-host.net:5000/api/';
 
 const $api = axios.create({
     withCredentials: true,

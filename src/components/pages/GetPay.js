8bcaf5c6235b3b558a4ef96aee4211c4d2b999cd
monosniper <vanteateam@gmail.com>
2021-12-05 17:10:35 +0500

    freekassa22

--- src/components/pages/GetPay.js
+++ src/components/pages/GetPay.js
@@ -1,11 +1,11 @@
 import React, {useContext, useEffect} from 'react';
-import {useSearchParams} from "react-router-dom";
+import {useParams, useSearchParams} from "react-router-dom";
 import {Context} from "../../index";
 
 const GetPay = () => {
 
     const {store} = useContext(Context);
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useParams();
 
     useEffect(() => {
         store.sendPay(searchParams);

50ea7673712ae962adc4d0c3b6750279b5d61dbf
monosniper <vanteateam@gmail.com>
2021-12-13 19:41:56 +0500

    paypal2

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import './scss/App.scss';
 import Header from "./components/layout/Header";
 import {Container} from "reactstrap";
 import Footer from "./components/layout/Footer";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
+const initialOptions = {
+    "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID,
+    intent: "capture",
+};
 
 function App() {
   return (
-      <BrowserRouter>
-          <Container>
-              <Header/>
-              <AppRouter/>
-          </Container>
-          <Footer />
-      </BrowserRouter>
+      <PayPalScriptProvider options={initialOptions}>
+          <BrowserRouter>
+              <Container>
+                  <Header/>
+                  <AppRouter/>
+              </Container>
+              <Footer />
+          </BrowserRouter>
+      </PayPalScriptProvider>
   );
 }
 

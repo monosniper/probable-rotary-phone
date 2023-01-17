eaacfbb717e331b53af7e5a81f6ea52f2049ef93
monosniper <vanteateam@gmail.com>
2023-01-17 19:22:54 +0500

    asdasd

--- src/components/pages/SuccessPay.js
+++ src/components/pages/SuccessPay.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ContentBlock from "../ContentBlock";
 import {AiFillCheckCircle} from "react-icons/all";
 import {useTranslation} from "react-i18next";
 import {useParams} from "react-router-dom";
 
 const SuccessPay = () => {
-    const { order_id } = useParams()
+    const params = useParams()
     const { t } = useTranslation();
+    useEffect(() => {
+        console.log(params)
+    }, [])
+    useEffect(() => {
+        console.log(params)
+        console.log(params.order_id)
+    }, [params])
     return (
         <ContentBlock style={{display: 'flex', gap: 20, justifyContent: 'center', alignItems: 'center', fontSize: 20}}>
             <AiFillCheckCircle style={{color: 'lightgreen', fontSize: 30}} />
             {/*{t('success_pay')}. Email for payment: accounting@market-advantage.net*/}
             Great! <br/>
-            Your Order ID: {order_id} <br/>
+            Your Order ID: {params.order_id} <br/>
             Now you have to include the order ID in the notes/memo field and make you payment to the email: <a href="mailto:accounting@market-advantage.net">accounting@market-advantage.net</a>
         </ContentBlock>
     );
